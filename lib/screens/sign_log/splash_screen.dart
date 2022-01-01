0c1aad9b90cdd83a4a10751f02e76ef2d89a3a08
Tareq HB <tareqhb01@gmail.com>
2022-01-01 14:07:42 +0200

    splashScreen.

---
+++ lib/screens/sign_log/splash_screen.dart
@@ -0,0 +1,74 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:my_app/constant.dart';
+import 'package:my_app/screens/Home/home.dart';
+
+class splashScreen extends StatefulWidget {
+  const splashScreen({Key? key}) : super(key: key);
+
+  @override
+  _splashScreenState createState() => _splashScreenState();
+}
+
+class _splashScreenState extends State<splashScreen> {
+  @override
+  void initState() {
+    super.initState();
+
+    Timer(Duration(seconds: 10), () {
+      if (x == 201) {
+        () => Navigator.pushNamed(context, 'fourth');
+        // () => Navigator.pushReplacement(
+        //   context,
+        //   MaterialPageRoute(
+        //     builder: (context) => 'fourth',
+        //   ),
+        // ),
+      } else {
+        print("erorr");
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topRight,
+            end: Alignment.bottomLeft,
+            colors: [charcoal, persian],
+          ),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Column(
+              children: const [
+                // Image.asset(
+                //   'assets/images/grocery.png',
+                //   width: 300.0,
+                //   height: 300.0,
+                // ),
+                Text(
+                  "A whole offerio store\n at your fingertips",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 18.0,
+                  ),
+                ),
+              ],
+            ),
+            CircularProgressIndicator()
+          ],
+        ),
+      ),
+    );
+  }
+}
